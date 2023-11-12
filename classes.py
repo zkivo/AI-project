f9c97222c0ba8ebbb603ddb3227d3bee935002b6
Marco Schivo <marcoschivo1@gmail.com>
2023-11-12 11:02:13 +0100

    ann almost done

--- classes.py
+++ classes.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import numpy as np
 from numpy.random import default_rng
 
@@ -12,9 +13,14 @@ class ANN:
     # size = L
     w = []
     
-    # it contains the output of each layer, even the input layer
-    # size = L + 1 (one is the input layer)
-    layer = []
+    # list of size of each out_layer
+    # size = L + 1
+    topology = []
+
+    # List of output of each out_layer
+    # First element is the input out_layer
+    # size = L + 1
+    out_layer = []
 
     # contains the norms of (expected output - output of the network)
     list_errors = []
@@ -27,22 +33,26 @@ class ANN:
             if i == 0:
                 continue
             else:
-                #a = default_rng(int(datetime.now().timestamp())).random((n_nodes,topology[i - 1] + 1))
-                a = default_rng(69).random((n_nodes,topology[i - 1] + 1))
+                a = default_rng(int(datetime.now().timestamp())).random((n_nodes,topology[i - 1] + 1)) * 2 - 1
+                # a = default_rng(69).random((n_nodes,topology[i - 1] + 1)) * 2 - 1
+                # a = np.random.rand(n_nodes, topology[i - 1] + 1) * 2 - 1
             self.w.append(a)
+            # print(self.w)
             ##print(self.w[i][:,0:])
 
     def forward(self, x : np.array) -> np.array:
-        del self.layer[:]
-        prev_out = x.T
-        self.layer.append(prev_out)
-        prev_out = np.insert(prev_out,0,1)
+        del self.out_layer[:]
+        prev_out = x
+        self.out_layer.append(prev_out)
+        prev_out = np.insert(prev_out, 0, 1)
         for m in self.w:
+            #print(np.array2string(m, separator=',', formatter={'float_kind':lambda x: "%.2f" % x}), np.array2string(prev_out, separator=',', formatter={'float_kind':lambda x: "%.2f" % x}))
             prev_out = np.dot(m, prev_out)
             prev_out = sigmoid(prev_out)
-            self.layer.append(prev_out)
-            prev_out = np.insert(prev_out,0,1).T
-        self.output = self.layer[-1]
+            self.out_layer.append(prev_out)
+            prev_out = np.insert(prev_out, 0, 1)
+        self.output = self.out_layer[-1]
+        #print(np.array2string(self.output, separator=','))
         return self.output
     
     # m is the matrix of the weights without the biases
@@ -57,16 +67,22 @@ class ANN:
             
 
     def fit_step(self, x : np.array, exp_out : np.array):
+        arr = np.zeros(self.topology[-1])
+        arr[int(exp_out)] = 1
+        exp_out = arr
         self.forward(x)
-        #self.list_errors.append(np.linalg.norm(exp_out - self.output)**2)
         delta = []
+        # print(self.output * (1 - self.output) * (exp_out - self.output))
         delta.append(self.output * (1 - self.output) * (exp_out - self.output))
-        for i, out in reversed(list(enumerate(self.layer))):
+        for l, out in reversed(list(enumerate(self.out_layer))):
             # outlayer
-            if i == len(self.layer) - 1 or i == 0: continue
+            if l == len(self.out_layer) - 1 or l == 0: continue
             # for j, node_out in out:
             #     delta.append(node_out * (1 - out) * )
-            delta.append(out * (1 - out) * self.sum_deltas(self.w[i][:,1:], delta[-1]))
+            for i, node in list(enumerate(out)):
+                pass
+            # print(out * (1 - out) * self.sum_deltas(self.w[l][:,1:], delta[-1]))
+            delta.append(out * (1 - out) * self.sum_deltas(self.w[l][:,1:], delta[-1]))
         # print(delta)
         delta = list(reversed(delta))
         for L,m in list(enumerate(self.w)):
@@ -75,6 +91,6 @@ class ANN:
                     if j == 0: #bias
                         self.w[L][i][j] += self.learning_rate * delta[L][i]
                     else:
-                        self.w[L][i][j] += self.learning_rate * delta[L][i] * self.layer[L][j - 1]
+                        self.w[L][i][j] += self.learning_rate * delta[L][i] * self.out_layer[L][j - 1]
 
 
--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from classes import ANN
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+
+
+def get_score():
+    rows = np.shape(X_test)[0]
+    succ = 0
+    for i in range(int(rows)):
+        out = ann.forward(X_test[i])
+        # print(X[i],out)
+        index = np.argmax(out)
+        if y_test[i] == index:
+            succ += 1
+    return succ / rows
+
+def shuffle(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
 
 ds = np.loadtxt("data/redwine-without-head.csv", delimiter=",")
 
-X = ds[::,:-1]
+shape = np.shape(ds)
+
+X = ds[:int(shape[0] * 0.8),:-1]
 X = X / np.max(X, axis=0)
-y = ds[::,-1]
+y = ds[:int(shape[0] * 0.8),-1]
+
+X_test = ds[int(shape[0] * 0.8):,:-1]
+y_test = ds[int(shape[0] * 0.8):, -1]
 
 for i,e in list(enumerate(y)):
     if e >= 6:
@@ -14,12 +37,36 @@ for i,e in list(enumerate(y)):
     else:
         y[i] = 0
 
-ann = ANN([11,5,5,1], 0.2)
+for i,e in list(enumerate(y_test)):
+    if e >= 6:
+        y_test[i] = 1
+    else:
+        y_test[i] = 0
 
-for e in range(5):
+# iris = load_iris()
+# X = np.array(iris.data)
+# y = np.array(iris.target)
+
+# X_test = X[int(np.shape(X)[0] * 0.8):,:]
+# y_test = y[int(np.shape(y)[0] * 0.8):]
+
+ann = ANN([11,16,16,2], 0.05)
+
+first_score = get_score()
+
+# epochs
+for _ in range(50):
+    # train the whole dataset
+    # X,y = shuffle(X,y)
     for i, x in list(enumerate(X)):
         ann.fit_step(np.array(x),np.array(y[i]))
-    ann.list_errors.append(np.linalg.norm(y[0] - ann.forward(np.array(X[0]))))
+    err = []
+    for i, x in list(enumerate(X)):
+        err.append(np.sum((y[i] - ann.forward(x))**2))
+    err = np.average(np.array(err))
+    ann.list_errors.append(err)
+
+print("Score = ", get_score(), " ( starting at", first_score, ")")
 
 fig, ax = plt.subplots()
 ax.plot(ann.list_errors)
